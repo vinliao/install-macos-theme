1629f50169319be15b8511e72aff00a4a2340f47
Vincent Liao <vincent.liao@protonmail.com>
2020-05-01 09:41:20 +0700

    check if root dnf

--- install-dependencies-dnf.sh
+++ install-dependencies-dnf.sh
@@ -1,1 +1,10 @@
-sudo dnf install sassc optipng inkscape libcanberra-gtk3 glib2-devel
+#!/bin/bash
+
+# check if root or not
+if [ `id -u` -ne 0 ]
+then
+  echo "Please run as root."
+  exit 1
+fi
+
+dnf install sassc optipng inkscape libcanberra-gtk3 glib2-devel
