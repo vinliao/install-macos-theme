f697cb3a50d817faee66c08509b9aeafe4fd9b16
Vincent Liao <vincent.liao@protonmail.com>
2020-05-01 18:26:10 +0700

    immediately set theme after install

--- install.sh
+++ install.sh
@@ -13,7 +13,31 @@ cd Mojave-gtk-theme
 cd ..
 
 echo
-echo "The themes and icons are already installed on your machine."
-echo "Now you can change it with gnome tweaks"
-echo "If you haven't installed gnome-tweaks yet, install it from your package manager"
-echo
+echo "The themes and icons have been installed on your machine"
+
+read -p "Do you want to change your theme now (y/n)? " CONT
+if [ "$CONT" = "y" ]; then
+    read -p "Which color scheme do you prefer (light/dark)? " COLOR
+    if [ "$COLOR" = "dark" ]; then
+        gsettings set org.gnome.desktop.interface gtk-theme "Mojave-dark"
+        gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle-dark"
+        gsettings set org.gnome.shell.extensions.user-theme name "Mojave-dark"
+
+    elif [ "$COLOR" = "light" ]; then
+        gsettings set org.gnome.desktop.interface gtk-theme "Mojave-light"
+        gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle"
+        gsettings set org.gnome.shell.extensions.user-theme name "Mojave-light"
+
+    else
+        echo "Unknown option. Setting it to dark..."
+        gsettings set org.gnome.desktop.interface gtk-theme "Mojave-dark"
+        gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle-dark"
+        gsettings set org.gnome.shell.extensions.user-theme name "Mojave-dark"
+
+    fi
+else
+    echo
+    echo "Alright then, you can change it on your own with gnome tweaks"
+    echo "If you haven't installed it yet, make sure to install it with your package manager"
+    echo
+fi
