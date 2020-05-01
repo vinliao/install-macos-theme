7ea82d00469fc569026911fa8e2cab6c20d036a9
Vincent Liao <vincent.liao@protonmail.com>
2020-05-01 15:14:35 +0700

    changed from fork repo to original repo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd McMojave-circle
 cd ..
 
 # install themes
-git clone https://github.com/vinliao/Mojave-gtk-theme.git
+git clone https://github.com/vinceliuice/Mojave-gtk-theme.git
 cd Mojave-gtk-theme
 ./install.sh -d ~/.themes
 cd ..
@@ -17,9 +17,3 @@ echo "The themes and icons are already installed on your machine."
 echo "Now you can change it with gnome tweaks"
 echo "If you haven't installed gnome-tweaks yet, install it from your package manager"
 echo
-
-# ask whether the user wants to change to the theme now (use gsettings.gnome.blablabla)
-# echo "Do you want to change the theme now? [y/N]"
-# # if y
-# gsettings set org.gnome.desktop.interface gtk-theme "Mojave"
-# gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle"
