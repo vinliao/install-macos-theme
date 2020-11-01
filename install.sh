80ea2cd2376978da54933a08258d4262b730ec7c
Vincent Liao <vincent.liao@protonmail.com>
2020-11-01 21:52:54 +0700

    update to whitesur

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # install icon
-git clone https://github.com/vinceliuice/McMojave-circle.git
-cd McMojave-circle
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+cd WhiteSur-icon-theme
 ./install.sh -d ~/.icons
 cd ..
 
 # install themes
-git clone https://github.com/vinceliuice/Mojave-gtk-theme.git
-cd Mojave-gtk-theme
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+cd WhiteSur-gtk-theme
 ./install.sh -d ~/.themes
 cd ..
 
@@ -19,20 +19,20 @@ read -p "Do you want to change your theme now (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
     read -p "Which color scheme do you prefer (light/dark)? " COLOR
     if [ "$COLOR" = "dark" ]; then
-        gsettings set org.gnome.desktop.interface gtk-theme "Mojave-dark"
-        gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle-dark"
-        gsettings set org.gnome.shell.extensions.user-theme name "Mojave-dark"
+        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark"
+        gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-dark"
+        gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-dark"
 
     elif [ "$COLOR" = "light" ]; then
-        gsettings set org.gnome.desktop.interface gtk-theme "Mojave-light"
-        gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle"
-        gsettings set org.gnome.shell.extensions.user-theme name "Mojave-light"
+        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-light"
+        gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-light"
+        gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-light"
 
     else
         echo "Unknown option. Setting it to dark..."
-        gsettings set org.gnome.desktop.interface gtk-theme "Mojave-dark"
-        gsettings set org.gnome.desktop.interface icon-theme "McMojave-circle-dark"
-        gsettings set org.gnome.shell.extensions.user-theme name "Mojave-dark"
+        gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark"
+        gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-dark"
+        gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-dark"
 
     fi
 else
